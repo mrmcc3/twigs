d8614dcb7167a5c7266560c7a053e62718be205c
mrmcc3 <mrmcc3@gmail.com>
2015-05-17 20:13:57 +1000

    Boilerplate clojure library from lein new

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject twigs "0.1.0-SNAPSHOT"
+  :description "A nicer way to use firebase in Clojure[Script]"
+  :url "https://github.com/mrmcc3/twigs"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
