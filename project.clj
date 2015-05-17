fe48ca50f386c0274a52254077ab119c4d8bc501
mrmcc3 <mrmcc3@gmail.com>
2015-05-17 20:20:34 +1000

    Added deps. Clojure 1.7.0-beta3, Clojurescript 3269 and Firebase 2.2.3 from cljsjs.

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "A nicer way to use firebase in Clojure[Script]"
   :url "https://github.com/mrmcc3/twigs"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.clojure/clojurescript "0.0-3269"]
+                 [cljsjs/firebase "2.2.3-0"]])
