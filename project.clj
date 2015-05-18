72c3df57a24540e49ecaeac1ec3d19511f1d62e6
mrmcc3 <mrmcc3@gmail.com>
2015-05-18 18:47:59 +1000

    remove repository info from project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twigs "0.1.3"
+(defproject twigs "0.1.4-SNAPSHOT"
   :description "A nicer way to use firebase in Clojure[Script]"
   :url "https://github.com/mrmcc3/twigs"
   :license {:name "MIT"}
@@ -7,9 +7,6 @@
                  [org.clojure/clojurescript "0.0-3269"]
                  [com.firebase/firebase-client-jvm "2.2.4"]
                  [cljsjs/firebase "2.2.3-0"]]
-
-  :repositories [["releases" {:url "https://clojars.org/repo"
-                              :creds :gpg}]]
 
   :plugins [[lein-cljsbuild "1.0.6"]]
 
