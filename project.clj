d5bad76d47e9767b582e96136a89561879ffefe9
mrmcc3 <mrmcc3@gmail.com>
2015-06-03 16:55:11 +1000

    releasing 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twigs "0.1.4"
+(defproject twigs "0.1.5"
   :description "A nicer way to use firebase in Clojure[Script]"
   :url "https://github.com/mrmcc3/twigs"
   :license {:name "MIT"}
