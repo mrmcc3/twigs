77485dce54b802cfb23a21bb1f19b6b43667682e
mrmcc3 <mrmcc3@gmail.com>
2015-06-10 18:22:14 +1000

    Version 0.1.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mrmcc3/twigs "0.1.9"
+(defproject mrmcc3/twigs "0.1.10-SNAPSHOT"
   :description "A simple way to use firebase in Clojure[Script]"
   :url "https://github.com/mrmcc3/twigs"
   :license {:name "MIT"}
